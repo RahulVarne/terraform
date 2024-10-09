cd7a56c55b5ee5a464b626b10924b33b11b5e0d6
RahulVarne <rahulvarne16@gmail.com>
2024-10-09 22:18:33 +0530

    Delete main.tf

--- main.tf
+++
@@ -1,7 +0,0 @@
-module "ec2_instance" {
-source        = "/mnt/f/Terraform/module/ec2.tf"
-ami           = "ami-0583d8c7a9c35822c"
-instance_type = "t2.micro"
-region        = "us-east-1"
-instance_name = "module-instance"
-}
