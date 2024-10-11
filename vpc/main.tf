d5959756c59eeafd808384bcf6166e1b3d5d7bea
RahulVarne <rahulvarne16@gmail.com>
2024-10-11 18:30:55 +0530

    Delete vpc directory

--- vpc/main.tf
+++
@@ -1,8 +0,0 @@
-module "vpc" {
-source ="/mnt/f/vpc/vpc.tf"
-region  = "us-east-1"
-cidr_block = "10.0.0.0/16"
-public_subnet_count = 3
-public_subnet_cidr = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-availability_zone = ["us-east-1a", "us-east-1b", "us-east-1c"]
-}
