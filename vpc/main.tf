fa27555f73fbfc2cde9fe4aa92a42fbd2ad945b4
RahulVarne <rahulvarne16@gmail.com>
2025-02-15 16:46:19 +0530

    Update main.tf

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,13 +1,15 @@
 module "vpc" {
-source ="git::https://github.com/RahulVarne/terraform.git//vpc/vpc.tf"
-vpc_name ="my vpc"
-region  = "us-east-1"
-cidr_block = "10.0.0.0/16"
-public_subnet_count = 3
-public_subnet_cidr = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-availability_zone = ["us-east-1a", "us-east-1b", "us-east-1c"]
-tags = {
-Environment = "dev"
-Team = "DevOps"
-}
+  source = "git::https://github.com/RahulVarne/terraform.git//vpc"
+
+  vpc_name            = "my vpc"
+  region              = "us-east-1"
+  cidr_block          = "10.0.0.0/16"
+  public_subnet_count = 3
+  public_subnet_cidr  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  availability_zones  = ["us-east-1a", "us-east-1b", "us-east-1c"]
+
+  tags = {
+    Environment = "dev"
+    Team        = "DevOps"
+  }
 }
